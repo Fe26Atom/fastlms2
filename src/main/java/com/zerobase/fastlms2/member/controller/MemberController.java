308c9559c49e5f5aaa784aeb5ab514be3650e4ee
DESKTOP-FP66TDU\Fe26Atom <fe26ming@gmail.com>
2021-10-16 22:35:19 +0900

    09-회원가입 _POST_SUBMIT된 내용_JPA를 통한 DB저장_2

--- src/main/java/com/zerobase/fastlms2/member/controller/MemberController.java
+++ src/main/java/com/zerobase/fastlms2/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.zerobase.fastlms2.member.model.MemberInput;
 import com.zerobase.fastlms2.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -23,11 +24,12 @@ public class MemberController {
 
 
     @PostMapping("/member/register")
-    public String registerSubmit(HttpServletRequest request
+    public String registerSubmit(Model model, HttpServletRequest request
         , MemberInput parameter) {
 
         boolean result = memberService.register(parameter);
 
+        model.addAttribute("result", result);
         return "member/register_complete";
     }
 }
--- src/main/java/com/zerobase/fastlms2/member/service/MemberServiceImpl.java
+++ src/main/java/com/zerobase/fastlms2/member/service/MemberServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,8 +15,19 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    /**
+     * 회원 가입
+     */
+
     @Override
     public boolean register(MemberInput parameter) {
+
+        Optional<Member> optionalMember = memberRepository.findById(parameter.getUserId());
+
+        if(optionalMember.isPresent()) {
+            //현재 userId에 해당하는 데이터 존재
+            return false;
+        }
 
         Member member = new Member();
         member.setUserId(parameter.getUserId());
@@ -24,7 +36,6 @@ public class MemberServiceImpl implements MemberService{
         member.setPassword(parameter.getPassword());
         member.setRegDt(LocalDateTime.now());
         memberRepository.save(member);
-
 
         return false;
     }
